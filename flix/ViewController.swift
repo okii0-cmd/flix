1a4d0e80c4fe94050cae11d651e6981540b5f146
okii0-cmd <63493883+okii0-cmd@users.noreply.github.com>
2021-09-18 13:03:46 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by okii on 18/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
